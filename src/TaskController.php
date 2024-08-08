6a86ae279d0149a9aff6236e2654e0edafa0d546
rod773 <rodrigoarcadio1@gmail.com>
2024-08-08 18:29:50 +0200

    update

--- src/TaskController.php
+++ src/TaskController.php
@@ -6,19 +6,27 @@ class TaskController{
     public function processRequest($method,$id){
 
         if($id ==  null){
-            echo "index";
+
+            if($method == 'GET'){
+                echo "index"; 
+            }
+           
+            elseif($method == 'POST'){
+                echo "create";
+                
+            }
+            else{
+                $this->respondMethodNotAllowed();
+            }
             
         }
-        elseif($method == 'POST'){
-            echo "create";
-            
-        }
+       
         else{
             switch($method){
                 case 'GET':
                     echo "show $id";
                     break;
-                    
+
                 case 'PATCH':
                     echo "update $id";
                     break;
